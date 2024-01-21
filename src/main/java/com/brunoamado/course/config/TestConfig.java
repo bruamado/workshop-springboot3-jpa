04c22f6b828693b708e06244cbbc1f7f0ac765ee
Bruno <bru-a@hotmail.com>
2024-01-20 21:14:38 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/brunoamado/course/config/TestConfig.java
+++ src/main/java/com/brunoamado/course/config/TestConfig.java
@@ -49,6 +49,15 @@ public class TestConfig implements CommandLineRunner{
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		p1.getCategories().add(c2);
+		p2.getCategories().add(c1);
+		p2.getCategories().add(c3);
+		p3.getCategories().add(c3);
+		p4.getCategories().add(c3);
+		p5.getCategories().add(c2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
